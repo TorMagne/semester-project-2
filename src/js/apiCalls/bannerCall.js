8e10e7aa759e6c71eba7dd4fcb759370773db831
Tor Magne <61709493+TorMagne@users.noreply.github.com>
2022-10-15 12:10:26 +0200

    removed some console.logs

--- src/js/apiCalls/bannerCall.js
+++ src/js/apiCalls/bannerCall.js
@@ -6,14 +6,12 @@ const bannerCall = async () => {
   try {
     const response = await fetch(heroBannerUrl);
     const json = await response.json();
-    console.log('response', response);
 
     const herroBanner = json.hero_banner.url;
 
     renderBanner(herroBanner);
   } catch (error) {
     displayMessage('warning', 'An error occured', '.message-container');
-    console.log('error call', error);
   }
 };
 
